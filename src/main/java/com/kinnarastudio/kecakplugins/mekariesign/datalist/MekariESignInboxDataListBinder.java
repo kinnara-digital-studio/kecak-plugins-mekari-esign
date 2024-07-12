14c1f061feb2068bf2333a0bbd9c674ec584c565
nathanael-adi-t <nathanael@kinnarastudio.com>
2024-07-12 15:47:33 +0700

    datalist and prop ok

--- src/main/java/com/kinnarastudio/kecakplugins/mekariesign/datalist/MekariESignInboxDataListBinder.java
+++ src/main/java/com/kinnarastudio/kecakplugins/mekariesign/datalist/MekariESignInboxDataListBinder.java
@@ -2,23 +2,31 @@ package com.kinnarastudio.kecakplugins.mekariesign.datalist;
 
 import java.text.ParseException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+import org.displaytag.properties.SortOrderEnum;
 import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.datalist.model.DataList;
 import org.joget.apps.datalist.model.DataListBinderDefault;
 import org.joget.apps.datalist.model.DataListCollection;
 import org.joget.apps.datalist.model.DataListColumn;
 import org.joget.apps.datalist.model.DataListFilterQueryObject;
+import org.joget.commons.util.LogUtil;
 import org.joget.plugin.base.PluginManager;
 
+import com.kinnarastudio.commons.mekarisign.model.AuthenticationToken;
 import com.kinnarastudio.commons.mekarisign.model.DocumentCategory;
 import com.kinnarastudio.commons.mekarisign.model.DocumentListResponse;
+import com.kinnarastudio.commons.mekarisign.model.GetDocumentListBody;
+import com.kinnarastudio.commons.mekarisign.model.ResponseData;
 import com.kinnarastudio.commons.mekarisign.model.ServerType;
 import com.kinnarastudio.commons.mekarisign.model.SigningStatus;
 import com.kinnarastudio.commons.mekarisign.model.StampingStatus;
+import com.kinnarastudio.commons.mekarisign.model.TokenType;
 import com.kinnarastudio.commons.mekarisign.service.DocumentListService;
 import com.kinnarastudio.commons.mekarisign.*;
 import com.kinnarastudio.commons.mekarisign.exception.BuildingException;
@@ -44,28 +52,62 @@ public class MekariESignInboxDataListBinder extends DataListBinderDefault {
     public DataListCollection getData(DataList dataList, Map map, DataListFilterQueryObject[] filterQueryObject, String sort,
             Boolean desc, Integer start, Integer rows) {
         MekariSign mekariSign;
+        DataListCollection <Map<String,String>> dataListCollection = new DataListCollection<>();
         try {
+            AuthenticationToken authToken = new AuthenticationToken(getPropertyString("accessToken"), TokenType.BEARER, 3600, getPropertyString("refreshToken"), ServerType.valueOf(getPropertyString("serverType")));
+
             mekariSign = MekariSign.getBuilder()
-                        .setClientId(getPropertyString("clientId"))
-                        .setClientSecret(getPropertyString("clientSecret"))
-                        .setServerType(ServerType.valueOf(getPropertyString("serverType")))
-                        .setSecretCode(getPropertyString("secretCode"))
-                        .build();
+                        .setAuthenticationToken(authToken)
+                        .authenticateAndBuild();
                     
-            mekariSign.getDoc(1, 10, DocumentCategory.valueOf(getPropertyString("documentCategory")), SigningStatus.valueOf(getPropertyString("signingStatus")), StampingStatus.valueOf(getPropertyString("stampingStatus")));
+            LogUtil.info(getClassName(), "Client ID: " + getPropertyString("clientId"));
+            LogUtil.info(getClassName(), "Client Secret: " + getPropertyString("clientSecret"));
+            LogUtil.info(getClassName(), "Server Type: " + ServerType.valueOf(getPropertyString("serverType")));
+            LogUtil.info(getClassName(), "Secret Code:" + getPropertyString("secretCode"));
+            
+            int page = (start / 10) + 1;
+            
+            GetDocumentListBody documentList = mekariSign.getDoc(page, rows, DocumentCategory.valueOf(getPropertyString("documentCategory")), SigningStatus.valueOf(getPropertyString("signingStatus")), StampingStatus.valueOf(getPropertyString("stampingStatus")));
+
+            ResponseData[] documents = documentList.getRespData();
+        
+            // Add each document to the DataListCollection
+            for (ResponseData document : documents) {
+                Map<String, String> maps = new HashMap<String,String>();
+                maps.put("id", document.getId());
+                maps.put("type", document.getType());
+                maps.put("filename", document.getAttributes().getFilename());
+                maps.put("category", document.getAttributes().getCategory().toString());
+                maps.put("docURL", document.getAttributes().getDocUrl());
+                dataListCollection.add(maps);
+            }
+            LogUtil.info(getClassName(), "List of DataListCollection: " + dataListCollection);
         } catch (BuildingException | RequestException | ParseException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        DataListCollection <String> id = new DataListCollection<>();
-        return id;
+        return dataListCollection;
     }
 
     @Override
     public int getDataTotalRowCount(DataList dataList, Map properties, DataListFilterQueryObject[] filterQueryObjects) {
         // TODO Auto-generated method stub
-        return 0;
+
+        MekariSign mekariSign;
+        
+        try {
+            AuthenticationToken authToken = new AuthenticationToken(getPropertyString("accessToken"), TokenType.BEARER, 3600, getPropertyString("refreshToken"), ServerType.valueOf(getPropertyString("serverType")));
+
+            mekariSign = MekariSign.getBuilder()
+                        .setAuthenticationToken(authToken)
+                        .authenticateAndBuild();
+            
+            GetDocumentListBody documentList = mekariSign.getDoc(1, 1, DocumentCategory.valueOf(getPropertyString("documentCategory")), SigningStatus.valueOf(getPropertyString("signingStatus")), StampingStatus.valueOf(getPropertyString("stampingStatus")));
+
+            return documentList.getDocListPagination().getDocumentCount();
+        } catch (BuildingException | RequestException | ParseException e) {
+            return 0;
+        }
     }
 
     @Override
@@ -91,7 +133,7 @@ public class MekariESignInboxDataListBinder extends DataListBinderDefault {
 
     @Override
     public String getDescription() {
-        return getClass().getPackage().getImplementationTitle();
+        return "kecak-plugins-mekari-esign";
     }
 
     @Override
