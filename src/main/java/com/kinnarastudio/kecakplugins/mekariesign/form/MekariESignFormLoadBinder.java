cbeac0035e3271c9673c69098cf756040f72425d
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2024-07-12 16:01:30 +0700

    add FormLoadBinder

---
+++ src/main/java/com/kinnarastudio/kecakplugins/mekariesign/form/MekariESignFormLoadBinder.java
@@ -0,0 +1,40 @@
+package com.kinnarastudio.kecakplugins.mekariesign.form;
+
+import org.joget.apps.form.model.*;
+
+public class MekariESignFormLoadBinder extends FormBinder implements FormLoadElementBinder {
+    @Override
+    public FormRowSet load(Element element, String s, FormData formData) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public String getVersion() {
+        return "";
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+    @Override
+    public String getLabel() {
+        return "";
+    }
+
+    @Override
+    public String getClassName() {
+        return "";
+    }
+
+    @Override
+    public String getPropertyOptions() {
+        return "";
+    }
+}
