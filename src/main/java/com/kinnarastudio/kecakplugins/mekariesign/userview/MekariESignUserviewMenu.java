a70a6a65174f60d4cbdd93bbd073faee19e33f2b
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2024-07-22 20:01:18 +0700

    add dataListDefinition

--- src/main/java/com/kinnarastudio/kecakplugins/mekariesign/userview/MekariESignUserviewMenu.java
+++ src/main/java/com/kinnarastudio/kecakplugins/mekariesign/userview/MekariESignUserviewMenu.java
@@ -1,24 +1,42 @@
 package com.kinnarastudio.kecakplugins.mekariesign.userview;
 
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.*;
+import java.util.stream.Stream;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import org.joget.apps.app.dao.DatalistDefinitionDao;
+import org.joget.apps.app.model.AppDefinition;
+import org.joget.apps.app.model.DatalistDefinition;
+import org.joget.apps.app.service.AppService;
 import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.datalist.model.*;
+import org.joget.apps.datalist.service.DataListService;
+import org.joget.apps.userview.model.Userview;
 import org.joget.apps.userview.model.UserviewMenu;
 import org.joget.commons.util.LogUtil;
+import org.joget.commons.util.ResourceBundleUtil;
+import org.joget.commons.util.TimeZoneUtil;
+import org.joget.directory.model.User;
+import org.joget.directory.model.service.DirectoryManager;
 import org.joget.plugin.base.PluginManager;
+import org.joget.workflow.model.WorkflowActivity;
+import org.joget.workflow.model.WorkflowAssignment;
+import org.joget.workflow.model.service.WorkflowManager;
+import org.joget.workflow.model.service.WorkflowUserManager;
 import org.joget.workflow.util.WorkflowUtil;
 import org.springframework.context.ApplicationContext;
 
 import com.kinnarastudio.commons.mekarisign.model.ServerType;
 
-public class MekariESignUserviewMenu extends UserviewMenu{
+public class MekariESignUserviewMenu extends UserviewMenu {
     public final static String LABEL = "Mekari eSign Userview Menu";
+
+    private DataList cachedDataList = null;
+
 
     @Override
     public String getClassName() {
@@ -86,6 +104,79 @@ public class MekariESignUserviewMenu extends UserviewMenu{
     }
 
     @Override
+    public String getJspPage() {
+        return getJspPage("userview/plugin/form.jsp", "userview/plugin/datalist.jsp", "userview/plugin/unauthorized.jsp");
+    }
+
+    protected String getJspPage(String jspFormFile, String jspListFile, String jspUnauthorizedFile) {
+        String mode = getRequestParameterString("_mode");
+        switch (mode) {
+            case "new":
+                return jspUnauthorizedFile;
+            default:
+                getJspDataList();
+                return jspListFile;
+        }
+    }
+
+
+    protected String getJspForm(String jspFormFile, String jspUnauthorizedFile) {
+        return jspUnauthorizedFile;
+    }
+
+    protected void getJspDataList() {
+        try {
+            DataList dataList = getDataList();
+            if (dataList != null) {
+                dataList.setCheckboxPosition("no");
+                DataListActionResult ac = dataList.getActionResult();
+                if (ac != null) {
+                    if (ac.getMessage() != null && !ac.getMessage().isEmpty()) {
+                        setAlertMessage(ac.getMessage());
+                    }
+                    if (ac.getType() != null && "REDIRECT".equals(ac.getType()) && ac.getUrl() != null && !ac.getUrl().isEmpty()) {
+                        if ("REFERER".equals(ac.getUrl())) {
+                            HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
+                            if (request != null && request.getHeader("Referer") != null) {
+                                setRedirectUrl(request.getHeader("Referer"));
+                            } else {
+                                setRedirectUrl("REFERER");
+                            }
+                        } else {
+                            setRedirectUrl(ac.getUrl());
+                        }
+                    }
+                }
+                setProperty("dataList", dataList);
+            } else {
+                setProperty("error", ("Data List \"" + getPropertyString("datalistId") + "\" not exist."));
+            }
+        } catch (Exception ex) {
+            StringWriter out = new StringWriter();
+            ex.printStackTrace(new PrintWriter(out));
+            String message = ex.toString();
+            message = message + "\r\n<pre class=\"stacktrace\">" + out.getBuffer() + "</pre>";
+            setProperty("error", message);
+        }
+    }
+
+    protected DataList getDataList() {
+        ApplicationContext ac = AppUtil.getApplicationContext();
+        DataListService dataListService = (DataListService) ac.getBean("dataListService");
+        if (cachedDataList == null) {
+            String dataListJson = AppUtil.readPluginResource(getClassName(), "/jsonDefinitions/mekariDataListBinder.json", null, true);
+            cachedDataList = dataListService.fromJson(dataListJson);
+            if (getPropertyString(Userview.USERVIEW_KEY_NAME) != null && getPropertyString(Userview.USERVIEW_KEY_NAME).trim().length() > 0) {
+                cachedDataList.addBinderProperty(Userview.USERVIEW_KEY_NAME, getPropertyString(Userview.USERVIEW_KEY_NAME));
+            }
+            if (getKey() != null && getKey().trim().length() > 0) {
+                cachedDataList.addBinderProperty(Userview.USERVIEW_KEY_VALUE, getKey());
+            }
+        }
+        return cachedDataList;
+    }
+
+    @Override
     public boolean isHomePageSupported() {
         return true;
     }
@@ -94,8 +185,7 @@ public class MekariESignUserviewMenu extends UserviewMenu{
         return (String) WorkflowUtil.getHttpServletRequest().getSession().getAttribute("MekariToken");
     }
 
-    public String getServerType()
-    {
+    public String getServerType() {
         return (String) WorkflowUtil.getHttpServletRequest().getSession().getAttribute("MekariServerType");
     }
 }
