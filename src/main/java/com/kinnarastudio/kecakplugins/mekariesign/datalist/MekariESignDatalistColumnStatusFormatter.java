4c62d623d73877d2daec93e4b6cb135c51a40d72
Natsuko Aiko Tsukiko <natsuchi@shiranai-localhost.shiranaigroup.net>
2024-08-12 21:31:15 +0700

    make datalist look like mekari app

---
+++ src/main/java/com/kinnarastudio/kecakplugins/mekariesign/datalist/MekariESignDatalistColumnStatusFormatter.java
@@ -0,0 +1,83 @@
+package com.kinnarastudio.kecakplugins.mekariesign.datalist;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.stream.Stream;
+
+import org.joget.apps.app.dao.PluginDefaultPropertiesDao;
+import org.joget.apps.app.model.AppDefinition;
+import org.joget.apps.app.model.PluginDefaultProperties;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.datalist.model.DataList;
+import org.joget.apps.datalist.model.DataListColumn;
+import org.joget.apps.datalist.model.DataListColumnFormatDefault;
+import org.joget.plugin.base.PluginManager;
+import org.joget.plugin.property.service.PropertyUtil;
+import com.kinnarastudio.kecakplugins.mekariesign.webservice.MekariESignWebhook;
+
+public class MekariESignDatalistColumnStatusFormatter extends DataListColumnFormatDefault{
+    public final static String LABEL = "Mekari eSign Datalist Column Status Formatter";
+
+    @Override
+    public String format(DataList dataList, DataListColumn dataListColumn, Object arg2, Object arg3) {
+        if (arg3.equals("in_progress"))
+        {
+            return "In Progress";
+        }
+        else
+        {
+            return "Completed";
+        }
+    }
+
+    @Override
+    public String getPropertyString(String property) {
+        PluginDefaultPropertiesDao pluginDefaultPropertiesDao = (PluginDefaultPropertiesDao) AppUtil.getApplicationContext().getBean("pluginDefaultPropertiesDao");
+        AppDefinition appDefinition = AppUtil.getCurrentAppDefinition();
+
+        return Optional.ofNullable(pluginDefaultPropertiesDao.getPluginDefaultPropertiesList(MekariESignWebhook.class.getName(), appDefinition, null, null, null, 1))
+                .map(Collection::stream)
+                .orElseGet(Stream::empty)
+                .findFirst()
+                .map(PluginDefaultProperties::getPluginProperties)
+                .map(PropertyUtil::getPropertiesValueFromJson)
+                .map(m -> m.get(property))
+                .map(String::valueOf)
+                .orElse(super.getPropertyString(property));
+    }
+
+    @Override
+    public String getClassName() {
+        return getClass().getName();
+    }
+
+    @Override
+    public String getLabel() {
+        return LABEL;
+    }
+
+    @Override
+    public String getPropertyOptions() {
+        return "";
+    }
+
+    @Override
+    public String getDescription() {
+        return getClass().getPackage().getImplementationTitle();
+    }
+
+    @Override
+    public String getName() {
+        return LABEL;
+    }
+
+    @Override
+    public String getVersion() {
+        PluginManager pluginManager = (PluginManager) AppUtil.getApplicationContext().getBean("pluginManager");
+        ResourceBundle resourceBundle = pluginManager.getPluginMessageBundle(getClassName(), "/messages/BuildNumber");
+        String buildNumber = resourceBundle.getString("buildNumber");
+        return buildNumber;
+    }
+    
+}
